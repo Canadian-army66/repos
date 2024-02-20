1f6e28ff9c690232e1dffc5efc291d2a75f32c7e
Canadian-army66 <142023388+Canadian-army66@users.noreply.github.com>
2024-02-20 10:41:57 -0600

    Initial commit

---
+++ HelloWorld_KohnenChristian/Program.cs
@@ -0,0 +1,18 @@
+﻿using System; //needed to use consose class//
+
+namespace HelloWorld_KohnenChristian //collection of classes for the project//
+{
+    internal class Program
+    {
+        static void Main(string[] args) //executed Main method to start //
+        {
+           
+            Console.WriteLine("What is yoru first name?"); // Asks for first name //
+            string firstName = Console.ReadLine(); // Assigns first name //
+            Console.WriteLine("What is yoru last name?"); // Asks for last name //
+            string lastName = Console.ReadLine(); // Assigns for last name //
+
+            Console.WriteLine($"Hello, {firstName} {lastName}!"); // Prints message with first and last name variables //
+        }
+    }
+}
