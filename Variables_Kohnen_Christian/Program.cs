404536edc8b459906a0a18586fef49918d6f84d0
Canadian-army66 <142023388+Canadian-army66@users.noreply.github.com>
2024-02-21 10:21:46 -0600

    made variable assignment
    variable code

---
+++ Variables_Kohnen_Christian/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Variables_Kohnen_Christian
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int favNumber; //declares int varable 
+            bool isJumping = false, isRunning = false; //sets bool variable isJumping and isRunning as faslse
+            float Nyumer; //declares float as Nyumer
+            favNumber = 55; //gives favNumber a number
+            Nyumer = 0.25f; //gives Myumer a number
+            const double finalGrade = 85.5; //declares and gives finalGrade an number
+
+            Console.WriteLine("My favorite number"favNumber); //all of then write a small message next to the variables
+            Console.WriteLine("Jumping?"isJumping);
+            Console.WriteLine("Running"isRunning);
+            Console.WriteLine("just a number"Nyumer);
+            Console.WriteLine("My final grade"finalGrade);
+        }
+    }
+}
