cc4dbbfe18ca91eee46de2df380e93a2e124b281
Canadian-army66 <142023388+Canadian-army66@users.noreply.github.com>
2024-02-23 08:35:04 -0600

    made Casting project

---
+++ Casting_Kohnen_Christian/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Casting_Kohnen_Christian
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
