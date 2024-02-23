bd929bbf5f2c54fb730e97c26193c40fba94d5b5
Canadian-army66 <142023388+Canadian-army66@users.noreply.github.com>
2024-02-23 09:38:08 -0600

    made monke

--- Casting_Kohnen_Christian/Program.cs
+++ Casting_Kohnen_Christian/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Monkey? True or False?"); //asks user a true or false question
+            bool isTrue = Convert.ToBoolean(Console.ReadLine()); //user responds true or false
+            Console.WriteLine($"of course, Monkey is {isTrue}!"); //prints response with user input
+
+            Console.WriteLine("How many Monkey?"); //asks user for a whole number
+            int favNyum = Convert.ToInt32(Console.ReadLine()); //user places a number specifically
+            Console.WriteLine($"There are going to be {favNyum} Monkey in your walls!"); //prints out responce with user input
         }
     }
 }
