7ef6c2ccab4f92d9fdb318438344646c04f88672
Canadian-army66 <142023388+Canadian-army66@users.noreply.github.com>
2024-03-04 09:29:03 -0600

    made cool C# code

---
+++ Strings_Kohnen_Christian/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Strings_Kohnen_Christian
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a character from your keyboard!");
+            char theirChar = Console.ReadKey().KeyChar;
+           
+            bool isLetter = char.IsLetter(theirChar);
+            Console.WriteLine($"\n {theirChar} is a {isLetter} letter!");
+
+
+            Console.WriteLine("Please write a string of words");
+            string sentence = Console.ReadLine();
+
+            Console.WriteLine("enter a word");
+            string word = Console.ReadLine();
+            bool isIn = sentence.Contains(word);
+
+            Console.WriteLine($"the word {word} in in  the phrase \"{sentence}\", this is {isIn}");
+        }
+    }
+}
