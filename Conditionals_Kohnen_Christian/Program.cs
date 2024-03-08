66cc654e1f394e53a71b10ca45aac58e80ca6384
Canadian-army66 <142023388+Canadian-army66@users.noreply.github.com>
2024-03-08 09:05:06 -0600

    something

---
+++ Conditionals_Kohnen_Christian/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Conditionals_Kohnen_Christian
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
