c2e731f83d5a21fb5611b0d998919810de6288bb
Canadian-army66 <142023388+Canadian-army66@users.noreply.github.com>
2024-03-27 09:17:33 -0500

    gg

--- Loops_Kohnen_Christian/Program.cs
+++ Loops_Kohnen_Christian/Program.cs
@@ -10,15 +10,15 @@ namespace Loops_Kohnen_Christian
             string sentence = Console.ReadLine(); //saves responce as "sentence"
             Console.WriteLine($" that sentence has {CountNumberOfSpaces(sentence)} spaces in it!"); //prints the result of CountNumberOfSpaces
 
-            Console.WriteLine("Please input a number!");
-            string number = Console.ReadLine();
-            Console.WriteLine($"the sum of those two digits are {SumOfDigit(number)}");
+            Console.WriteLine("Please input a number!"); //asks user to place a number
+            string number = Console.ReadLine(); //saves responce as number
+            Console.WriteLine($"the sum of those two digits are {SumOfDigit(number)}"); //writes sum of the loop
         }
 
-        static int CountNumberOfSpaces(string sentence) //
+        static int CountNumberOfSpaces(string sentence) //makes the parameters for "CountNumberOfSpaces"
         {
             int numberOfSpaces = 0; //sets number of spaces as zero before loop
-            foreach (char character in sentence) //
+            foreach (char character in sentence) //sets up loop for to check the sentence 
             {
                 if (character == ' ') //looks for sapaces in the sentence and adds to the variable "numberOfSpaces" by one
                 {
@@ -28,14 +28,14 @@ namespace Loops_Kohnen_Christian
             return numberOfSpaces; //breaks loop
         }
 
-        static int SumOfDigit(string number)
+        static int SumOfDigit(string number) //makes the parameters for "SumOfDigit"
         {
-            int sum = 0;
-            foreach (char digit in number)
+            int sum = 0; //sets the number as zero before loop
+            foreach (char digit in number) //sets up loop to check digits
             {
-                sum += (int)Char.GetNumericValue(digit);
+                sum += (int)Char.GetNumericValue(digit); //adds the value of the digits to the sum
             }
-            return sum;
+            return sum; //breaks loop
         }
     }
 }
