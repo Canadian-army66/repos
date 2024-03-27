a2d86f16a32f1edfc31d6048a941df4be006fbf9
Canadian-army66 <142023388+Canadian-army66@users.noreply.github.com>
2024-03-27 09:14:21 -0500

    done did the requirements

--- Loops_Kohnen_Christian/Program.cs
+++ Loops_Kohnen_Christian/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Loops_Kohnen_Christian
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Loops_Kohnen_Christian
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Do you want to make games?"); //asks the user a question
+            string sentence = Console.ReadLine(); //saves responce as "sentence"
+            Console.WriteLine($" that sentence has {CountNumberOfSpaces(sentence)} spaces in it!"); //prints the result of CountNumberOfSpaces
+
+            Console.WriteLine("Please input a number!");
+            string number = Console.ReadLine();
+            Console.WriteLine($"the sum of those two digits are {SumOfDigit(number)}");
+        }
+
+        static int CountNumberOfSpaces(string sentence) //
+        {
+            int numberOfSpaces = 0; //sets number of spaces as zero before loop
+            foreach (char character in sentence) //
+            {
+                if (character == ' ') //looks for sapaces in the sentence and adds to the variable "numberOfSpaces" by one
+                {
+                    numberOfSpaces++;
+                }
+            }
+            return numberOfSpaces; //breaks loop
+        }
+
+        static int SumOfDigit(string number)
+        {
+            int sum = 0;
+            foreach (char digit in number)
+            {
+                sum += (int)Char.GetNumericValue(digit);
+            }
+            return sum;
         }
     }
 }
