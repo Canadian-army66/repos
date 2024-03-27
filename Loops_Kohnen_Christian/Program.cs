970f7e5e652f17b75188790ff617e2292e85e2c3
Canadian-army66 <142023388+Canadian-army66@users.noreply.github.com>
2024-03-27 08:42:12 -0500

    made project

---
+++ Loops_Kohnen_Christian/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Loops_Kohnen_Christian
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
